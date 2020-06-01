f1859fe75b8c0de6f00ca41cfd57c5fbef990585
yougoforward <908865817@qq.com>
2020-06-01 08:33:00 +0800

    gs4

--- experiments/segmentation/scripts/new_psp3_noatt_res50_pcontext.sh
+++ experiments/segmentation/scripts/new_psp3_noatt_res50_pcontext.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
 # train
-python train.py --dataset pcontext \
+python -m experiments.segmentation.train --dataset pcontext \
     --model new_psp3_noatt --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet50 --checkname new_psp3_noatt_res50_pcontext
 
 #test [single-scale]
-python test.py --dataset pcontext \
+python -m experiments.segmentation.test --dataset pcontext \
     --model new_psp3_noatt --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume runs/pcontext/new_psp3_noatt/new_psp3_noatt_res50_pcontext/model_best.pth.tar --split val --mode testval
+    --backbone resnet50 --resume experiments/segmentation/runs/pcontext/new_psp3_noatt/new_psp3_noatt_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python test.py --dataset pcontext \
+python -m experiments.segmentation.test --dataset pcontext \
     --model new_psp3_noatt --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume runs/pcontext/new_psp3_noatt/new_psp3_noatt_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
+    --backbone resnet50 --resume experiments/segmentation/runs/pcontext/new_psp3_noatt/new_psp3_noatt_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
