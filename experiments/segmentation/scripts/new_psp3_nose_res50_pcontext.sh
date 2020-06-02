8bf46df2fe0e7a6ccccc0236ea45b72cc3720a04
yougoforward <908865817@qq.com>
2020-06-02 14:52:45 +0800

    gs4

--- experiments/segmentation/scripts/new_psp3_nose_res50_pcontext.sh
+++ experiments/segmentation/scripts/new_psp3_nose_res50_pcontext.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
-# train
-python -m experiments.segmentation.train --dataset pcontext \
-    --model new_psp3_nose --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --checkname new_psp3_nose_res50_pcontext
+# # train
+# python -m experiments.segmentation.train --dataset pcontext \
+#     --model new_psp3_nose --aux --dilated --base-size 520 --crop-size 520 \
+#     --backbone resnet50 --checkname new_psp3_nose_res50_pcontext
 
 #test [single-scale]
 python -m experiments.segmentation.test --dataset pcontext \
     --model new_psp3_nose --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume experiments/segmentation/runs/pcontext/new_psp3_nose/new_psp3_nose_res50_pcontext/model_best.pth.tar --split val --mode testval
 
-#test [multi-scale]
-python -m experiments.segmentation.test --dataset pcontext \
-    --model new_psp3_nose --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume experiments/segmentation/runs/pcontext/new_psp3_nose/new_psp3_nose_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
+# #test [multi-scale]
+# python -m experiments.segmentation.test --dataset pcontext \
+#     --model new_psp3_nose --aux --dilated --base-size 520 --crop-size 520 \
+#     --backbone resnet50 --resume experiments/segmentation/runs/pcontext/new_psp3_nose/new_psp3_nose_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
