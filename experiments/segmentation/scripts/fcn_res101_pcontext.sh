a259c295ac2baaf36aa6a871cb2c92f6af09d858
yougoforward <908865817@qq.com>
2020-06-02 17:03:25 +0800

    gs4

---
+++ experiments/segmentation/scripts/fcn_res101_pcontext.sh
@@ -0,0 +1,15 @@
+# !/usr/bin/env bash
+# train
+python -m experiments.segmentation.train --dataset pcontext \
+    --model fcn --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --checkname fcn_res101_pcontext
+
+#test [single-scale]
+python -m experiments.segmentation.test --dataset pcontext \
+    --model fcn --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --resume experiments/segmentation/runs/pcontext/fcn/fcn_res101_pcontext/model_best.pth.tar --split val --mode testval
+
+#test [multi-scale]
+python -m experiments.segmentation.test --dataset pcontext \
+    --model fcn --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --resume experiments/segmentation/runs/pcontext/fcn/fcn_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
