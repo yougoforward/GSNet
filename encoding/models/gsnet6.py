ee436b194bf84898948ecb40a395ba4ac8463e06
yougoforward <908865817@qq.com>
2020-06-12 08:38:22 +0800

    gsnet8

--- encoding/models/gsnet6.py
+++ encoding/models/gsnet6.py
@@ -39,7 +39,7 @@ class gsnet6NetHead(nn.Module):
         inter_channels = in_channels // 4
 
         self.aa_gsnet6 = gsnet6_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
-        self.conv8 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(inter_channels+inter_channels//2, out_channels, 1))
+        self.conv8 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(inter_channels+inter_channels, out_channels, 1))
         if self.se_loss:
             self.selayer = nn.Linear(inter_channels, out_channels)
 
@@ -121,8 +121,13 @@ class gsnet6_Module(nn.Module):
         self.sem_gp = nn.Sequential(nn.Conv2d(out_channels, out_channels//2, 1, bias=False),
                             norm_layer(out_channels//2),
                             nn.ReLU(True))
-        self.se = nn.Sequential(
-                            nn.Conv2d(out_channels, out_channels, 1, bias=True),
+        # self.se = nn.Sequential(
+        #                     nn.Conv2d(out_channels, out_channels, 1, bias=True),
+        #                     nn.Sigmoid())
+        self.se = nn.Sequential(nn.Conv2d(out_channels, out_channels//2, 1, bias=True),
+                            norm_layer(out_channels//2),
+                            nn.ReLU(True),
+                            nn.Conv2d(out_channels//2, out_channels, 1, bias=True),
                             nn.Sigmoid())
 
         self.pam0 = PAM_Module(in_dim=out_channels, key_dim=out_channels//8,value_dim=out_channels,out_dim=out_channels,norm_layer=norm_layer)
@@ -152,7 +157,7 @@ class gsnet6_Module(nn.Module):
         #non-local
         out = self.pam0(out)
 
-        out = torch.cat([out, self.sem_gp(gp).expand(n, c//2, h, w)], dim=1)
+        out = torch.cat([out, gp.expand(n, c, h, w)], dim=1)
         return out, gp
 
 def get_gsnet6net(dataset='pascal_voc', backbone='resnet50', pretrained=False,
