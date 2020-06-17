db46a8ad85d6cf010bd41e025287c3473bd32778
yougoforward <908865817@qq.com>
2020-06-17 18:36:56 +0800

    mlgsnet101

--- experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
+++ experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
@@ -11,5 +11,5 @@
 
 #test [multi-scale]
 python -m experiments.segmentation.test --dataset pcontext \
-    --model new_psp3 --aux --dilated --base-size 576 --crop-size 520 \
+    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet101 --resume experiments/segmentation/runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best1.pth.tar --split val --mode testval --ms
