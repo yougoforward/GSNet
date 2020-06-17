ce1e92dc7156842f078d18e2cc47246c56b9159b
yougoforward <908865817@qq.com>
2020-06-17 17:14:20 +0800

    np3576

--- experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
+++ experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
-# train
-python -m experiments.segmentation.train --dataset pcontext \
-    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --checkname new_psp3_res101_pcontext
+# # train
+# python -m experiments.segmentation.train --dataset pcontext \
+#     --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
+#     --backbone resnet101 --checkname new_psp3_res101_pcontext
 
-#test [single-scale]
-python -m experiments.segmentation.test --dataset pcontext \
-    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet101 --resume experiments/segmentation/runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval
+# #test [single-scale]
+# python -m experiments.segmentation.test --dataset pcontext \
+#     --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
+#     --backbone resnet101 --resume experiments/segmentation/runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test --dataset pcontext \
-    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
+    --model new_psp3 --aux --dilated --base-size 576 --crop-size 520 \
     --backbone resnet101 --resume experiments/segmentation/runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
