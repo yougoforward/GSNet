22aac3827753e94b9a42b2e72f7452a20c499f39
yougoforward <908865817@qq.com>
2020-06-20 16:11:26 +0800

    first commit

--- experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
+++ experiments/segmentation/scripts/new_psp3_res101_pcontext.sh
@@ -4,10 +4,10 @@
 #     --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
 #     --backbone resnet101 --checkname new_psp3_res101_pcontext
 
-# #test [single-scale]
-# python -m experiments.segmentation.test --dataset pcontext \
-#     --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
-#     --backbone resnet101 --resume experiments/segmentation/runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval
+#test [single-scale]
+python -m experiments.segmentation.test --dataset pcontext \
+    --model new_psp3 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --resume experiments/segmentation/runs/pcontext/new_psp3/new_psp3_res101_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset pcontext \
