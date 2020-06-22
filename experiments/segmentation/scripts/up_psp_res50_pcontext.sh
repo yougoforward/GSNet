f1818784aee85680e250fdf2aadcf803446dc3a5
yougoforward <908865817@qq.com>
2020-06-22 17:40:41 +0800

    first commit

--- experiments/segmentation/scripts/up_psp_res50_pcontext.sh
+++ experiments/segmentation/scripts/up_psp_res50_pcontext.sh
@@ -5,11 +5,11 @@ python -m experiments.segmentation.train --dataset pcontext \
     --backbone resnet50 --checkname up_psp_res50_pcontext
 
 #test [single-scale]
-python -m experiments.segmentation.test --dataset pcontext \
+python -m experiments.segmentation.test_whole --dataset pcontext \
     --model up_psp --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume experiments/segmentation/runs/pcontext/up_psp/up_psp_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python -m experiments.segmentation.test --dataset pcontext \
+python -m experiments.segmentation.test_whole --dataset pcontext \
     --model up_psp --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume experiments/segmentation/runs/pcontext/up_psp/up_psp_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
