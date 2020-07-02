812265a630388d8b5cb6cc590ee15ef7dfe3c724
yougoforward <908865817@qq.com>
2020-07-02 13:56:07 +0800

    new_gsnet

---
+++ experiments/segmentation/scripts/new_gsnet2_res101_pcontext.sh
@@ -0,0 +1,15 @@
+# !/usr/bin/env bash
+# train
+python -m experiments.segmentation.train --dataset pcontext \
+    --model new_gsnet2 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --checkname new_gsnet2_res101_pcontext
+
+#test [single-scale]
+python -m experiments.segmentation.test_whole --dataset pcontext \
+    --model new_gsnet2 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --resume experiments/segmentation/runs/pcontext/new_gsnet2/new_gsnet2_res101_pcontext/model_best.pth.tar --split val --mode testval
+
+#test [multi-scale]
+python -m experiments.segmentation.test_whole --dataset pcontext \
+    --model new_gsnet2 --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --resume experiments/segmentation/runs/pcontext/new_gsnet2/new_gsnet2_res101_pcontext/model_best1.pth.tar --split val --mode testval --ms
