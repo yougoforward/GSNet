f5c974ab824387e51f5ad353832773ca6a6618e6
yougoforward <908865817@qq.com>
2020-07-03 21:06:04 +0800

    first commit

--- experiments/segmentation/scripts/fcn_res101_pcontext.sh
+++ experiments/segmentation/scripts/fcn_res101_pcontext.sh
@@ -5,11 +5,11 @@
 #     --backbone resnet101 --checkname fcn_res101_pcontext
 
 #test [single-scale]
-python -m experiments.segmentation.test --dataset pcontext \
+python -m experiments.segmentation.test_whole --dataset pcontext \
     --model fcn --aux --dilated --base-size 520 --crop-size 520 \
     --backbone resnet101 --resume experiments/segmentation/runs/pcontext/fcn/fcn_res101_pcontext/model_best.pth.tar --split val --mode testval
 
 # #test [multi-scale]
-# python -m experiments.segmentation.test --dataset pcontext \
-#     --model fcn --aux --dilated --base-size 520 --crop-size 520 \
-#     --backbone resnet101 --resume experiments/segmentation/runs/pcontext/fcn/fcn_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
+python -m experiments.segmentation.test_whole --dataset pcontext \
+    --model fcn --aux --dilated --base-size 520 --crop-size 520 \
+    --backbone resnet101 --resume experiments/segmentation/runs/pcontext/fcn/fcn_res101_pcontext/model_best.pth.tar --split val --mode testval --ms
